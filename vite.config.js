3a9732b80d1c5d5f5dbad416fab30f476d1438fd
shubh kamra <shubhkamra19@gmail.com>
2025-07-23 10:26:05 +0000

    fixed proxy backend issue

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,13 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 
+const backendUrl = import.meta.env.VITE_BACKEND_URL || 'http://localhost:3000';
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'https://hideout-be.brazilsouth.cloudapp.azure.com',
+        target: `${backendUrl}`,
         secure: false,
       },
     },
